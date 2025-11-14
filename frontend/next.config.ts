2c33be4550b36e16efdc7857ecb5060e3af608af
Lance He <indigofeather@gmail.com>
2025-11-14 14:37:04 +0800

    fix: next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
